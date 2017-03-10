0026e21f43763c95f173e3f2c4e1657b35aae1f8
sonbn-225 <sonbn.225@gmail.com>
2017-03-11 04:50:39 +0700

    Update DAOdb

--- app/src/main/java/com/example/vaio/timestone/database/DAOdb.java
+++ app/src/main/java/com/example/vaio/timestone/database/DAOdb.java
@@ -1,7 +1,15 @@
 package com.example.vaio.timestone.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.util.Log;
+
+import com.example.vaio.timestone.model.Item;
+
+import java.util.ArrayList;
 
 /**
  * Created by sonbn on 3/11/17.
@@ -21,5 +29,52 @@ public class DAOdb {
      */
     public void close() {
         dbHelper.close();
+    }
+
+    public void insertData(ArrayList<Item> items){
+        String sql = "INSERT INTO "+ DBhelper.TB_NAME +" VALUES (?,?,?,?,?,?,?,?,?);";
+        SQLiteStatement statement = database.compileStatement(sql);
+        database.beginTransaction();
+        for (Item item : items) {
+            statement.clearBindings();
+            statement.bindString(2, item.getE_type());
+            statement.bindString(3, item.getE_info());
+            statement.bindString(4, item.getE_date());
+            statement.bindString(5, item.getE_day());
+            statement.bindString(6, item.getE_month());
+            statement.bindString(7, item.getE_year());
+            statement.bindLong(8, item.getE_weight());
+            statement.bindString(9, item.getUrl());
+            statement.execute();
+        }
+        database.setTransactionSuccessful();
+        database.endTransaction();
+    }
+
+    public ArrayList<Item> getData() {
+        ArrayList<Item> arrItem = new ArrayList<>();
+        Cursor cursor = database.query(DBhelper.TB_NAME, null, null, null, null, null, null);
+        int typeIndex = cursor.getColumnIndex(DBhelper.TYPE);
+        int infoIndex = cursor.getColumnIndex(DBhelper.INFO);
+        int dateIndex = cursor.getColumnIndex(DBhelper.DATE);
+        int dayIndex = cursor.getColumnIndex(DBhelper.DAY);
+        int monthIndex = cursor.getColumnIndex(DBhelper.MONTH);
+        int yearIndex = cursor.getColumnIndex(DBhelper.YEAR);
+        int weightIndex = cursor.getColumnIndex(DBhelper.WEIGHT);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            String type = cursor.getString(typeIndex);
+            String info = cursor.getString(infoIndex);
+            String date = cursor.getString(dateIndex);
+            String day = cursor.getString(dayIndex);
+            String month = cursor.getString(monthIndex);
+            String year = cursor.getString(yearIndex);
+            int weight = cursor.getInt(weightIndex);
+            Item item = new Item(type, info, date, day, month, year, weight, "");
+            arrItem.add(item);
+            Log.e("INFO", String.valueOf(arrItem.size()));
+            cursor.moveToNext();
+        }
+        return arrItem;
     }
 }
